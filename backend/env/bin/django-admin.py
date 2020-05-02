d53ab6d6bbbab9a6aaae57ae3b2d234a0636b9f7
62991856 <scottwillis@Scotts-MacBook-Pro.local>
2020-05-02 23:57:55 +0200

    template

---
+++ backend/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/scottwillis/Documents/Coding/Websites/scottwillis/backend/src/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

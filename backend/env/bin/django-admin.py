4a359f64391e308d76ac16ed2932522a6fbe1e6f
Bistrologist <scottwillis@Scotts-MacBook-Pro.local>
2020-05-03 17:38:22 +0200

    memory cut for pa

--- backend/env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/scottwillis/Documents/Coding/Websites/scottwillis/backend/src/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()

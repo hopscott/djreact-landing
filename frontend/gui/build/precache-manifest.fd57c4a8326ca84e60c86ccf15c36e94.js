8ea51021feee9232d839ec3376b5400725983a5f
Bistrologist <scottwillis@Scotts-MacBook-Pro.local>
2020-05-03 16:31:31 +0200

    frontend fix

---
+++ frontend/gui/build/precache-manifest.fd57c4a8326ca84e60c86ccf15c36e94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c329e83b5355f55d48ea653d68687fc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef67ff2a397cd43dc73e",
+    "url": "/static/css/2.35fcc229.chunk.css"
+  },
+  {
+    "revision": "7ee9b40523371d24c1ae",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ef67ff2a397cd43dc73e",
+    "url": "/static/js/2.4245004a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4245004a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ee9b40523371d24c1ae",
+    "url": "/static/js/main.b57a5ae5.chunk.js"
+  },
+  {
+    "revision": "c36fcdef3208a954cd18",
+    "url": "/static/js/runtime-main.ee2b0bef.js"
+  }
+]);
